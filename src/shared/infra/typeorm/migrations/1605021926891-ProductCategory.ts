0674f73c46f9103a82eb0dbcbe6e0ed6349f4a8e
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-11-10 14:19:01 -0300

    Add migrations de products e products category

---
+++ src/shared/infra/typeorm/migrations/1605021926891-ProductCategory.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class ProdCategory1605021926891 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products_category',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('product_category');
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1605027556174-Product.ts
@@ -0,0 +1,89 @@
+import { query } from 'express';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class Product1605027556174 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'size',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_products-products_category',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products_category',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'products',
+      'fk_products-products_category'
+    );
+    await queryRunner.dropTable('products');
+  }
+}
