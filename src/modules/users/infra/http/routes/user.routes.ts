5118af9ccc06bda92845092644e4deff16edae05
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-11-14 09:39:59 -0300

    fix: add suport to gender in user create

--- src/modules/users/infra/http/routes/user.routes.ts
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -21,6 +21,7 @@ usersRouter.post(
       name: Joi.string().required(),
       email: Joi.string().email().required(),
       password: Joi.string().required(),
+      gender: Joi.string(),
     },
   }),
   usersController.create
