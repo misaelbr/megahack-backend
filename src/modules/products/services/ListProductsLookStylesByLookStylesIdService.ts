e653e01ae3cc043226b773d6bd76cf91868348b1
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-11-14 12:14:50 -0300

    refactor: add Object.assign to map only products in service return

--- src/modules/products/services/ListProductsLookStylesByLookStylesIdService.ts
+++ src/modules/products/services/ListProductsLookStylesByLookStylesIdService.ts
@@ -4,6 +4,7 @@ import ProductsLookStyles from '@modules/products/infra/typeorm/entities/Product
 
 import IProductsLookStylesRepository from '@modules/products/repositories/IProductsLookStylesRepository';
 import Product from '../infra/typeorm/entities/Product';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   look_styles_id: string;
@@ -18,40 +19,19 @@ class ListProductsLookStylesByLookStylesIdService {
 
   public async execute({
     look_styles_id,
-  }: IRequest): Promise<
-    | Omit<
-        Product,
-        'category' | 'order_products' | 'product_look_styles' | 'getimageUrl'
-      >[]
-    | undefined
-  > {
+  }: IRequest): Promise<Product[] | undefined> {
     const productsLookStylesList = await this.productsLookStylesRepository.findByLookStylesId(
       look_styles_id
     );
 
     if (productsLookStylesList) {
-      const products = productsLookStylesList.map<
-        Omit<
-          Product,
-          'category' | 'order_products' | 'product_look_styles' | 'getimageUrl'
-        >
-      >(productLookStyles => ({
-        id: productLookStyles.product_id,
-        name: productLookStyles.product.name,
-        description: productLookStyles.product.description,
-        color: productLookStyles.product.color,
-        gender: productLookStyles.product.gender,
-        size: productLookStyles.product.size,
-        price: productLookStyles.product.price,
-        brand: productLookStyles.product.brand,
-        category_id: productLookStyles.product.category_id,
-        quantity: productLookStyles.product.quantity,
-        image: productLookStyles.product.image,
-        created_at: productLookStyles.product.created_at,
-        updated_at: productLookStyles.product.updated_at,
-      }));
+      const products = productsLookStylesList.map<Product>(
+        productLookStyles => {
+          return Object.assign(productLookStyles.product);
+        }
+      );
 
-      return products;
+      return classToClass(products);
     }
 
     return undefined;
